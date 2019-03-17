aa0e291c874f28099b0df9deaaf72c179733061b
TyCoding <2557988481@qq.com>
2019-03-17 08:45:49 +0800

    :bookmark: 完善权限管理

--- src/main/java/cn/tycoding/ShiroApplication.java
+++ src/main/java/cn/tycoding/ShiroApplication.java
@@ -3,10 +3,17 @@ package cn.tycoding;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("cn.tycoding.*.mapper")
-public class ShiroApplication {
+public class ShiroApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ShiroApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ShiroApplication.class, args);
